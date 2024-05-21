cc4f9e62914b7bd1b2e09e7416ac861bf779aec3
Shawn <vuyuxl199199@gmail.com>
2024-05-21 17:25:19 +0800

    set postgresql string properly

--- backend/src/config.py
+++ backend/src/config.py
@@ -12,4 +12,5 @@ class Config:
     NOTION_CLIENT_ID = os.environ["NOTION_CLIENT_ID"]
     NOTION_SECRET = os.environ["NOTION_SECRET"]
     NOTION_TEST_DATABASE = os.environ.get("NOTION_TEST_DATABASE")
-    POSTGRES_URL = os.environ.get("POSTGRES_URL")
+    # vercel 的 postgresql 的链接路径是 postgres:// 开头，但是 sqlalchemy 要求是 postgresql:// 开头，替换一下即可
+    POSTGRES_URL = os.environ.get("POSTGRES_URL").replace("postgres://", "postgresql://")
