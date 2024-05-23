b50ee49d9bba8232ae7d232e7f0c6f376a1a58b5
Shawn <vuyuxl199199@gmail.com>
2024-05-23 11:36:18 +0800

    小改善

--- frontend/src/components/NotionAuth.vue
+++ frontend/src/components/NotionAuth.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 const isLoggedIn = defineModel('isLoggedIn', { default: false });
+// 正在登录/授权中（用户点击了授权，但是要等待1-2s 的网络时间）
+const isAuthing = ref(false);
 
 const emit = defineEmits(['auth-success', 'log-out']);
 
 async function handleNotionAuth() {
+  isAuthing.value = true;
   try {
     await chrome.runtime.sendMessage({ message: 'notion-auth' });
     isLoggedIn.value = true;
@@ -13,6 +18,7 @@ async function handleNotionAuth() {
     // 第一种情况可以不用处理。第二种情况需要提示用户，但暂时先不处理
     console.log(error);
   }
+  isAuthing.value = false;
 }
 
 function logout() {
@@ -27,6 +33,7 @@ function logout() {
     :label="isLoggedIn ? 'Authorize Databases' : 'Log in to Proceed'"
     @click="handleNotionAuth"
     rounded
+    :loading="isAuthing"
   ></q-btn>
   <span class="q-mx-sm"></span>
   <q-btn color="purple" label="Log out" @click="logout" v-show="isLoggedIn" outline rounded></q-btn>
